2400da9f91163adca1e677cb50113173692c95e3
Benjamin Summers <ben@benjaminsummers.net>
2015-04-17 02:11:48 +0000

    Introduced some errors and other interesting behaviors.

--- src/Hello.hs
+++ src/Hello.hs
@@ -1,2 +1,14 @@
-module Hello where
-hello = putStrLn "hello world!"
+module Hello(hello,exported) where
+
+import qualified Module.That.Does.Not.Exist as DNE
+
+exported = putStrLn "hello world!"
+
+global = putStrLn "hello world!"
+
+hello = do
+  let local = putStrLn "hello world!"
+  global
+  exported
+  local
+  DNE.return ()
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,9 @@
-module Main where
+module Main(main,hello) where
 
 import qualified Hello
+import qualified Hello as H
+import           Hello(exported)
+
+hello = Hello.hello
 
 main = Hello.hello
